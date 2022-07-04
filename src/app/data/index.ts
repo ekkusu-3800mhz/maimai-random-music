7eed7bbcf55d4a8a3cb438356f7fb58b7514c4e6
Shinra DX <shinra.dx@outlook.com>
2022-07-05 01:00:38 +0800

    废除过时的歌曲封面获取方式

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -5,17 +5,11 @@ export default class Data {
 
     private static _dataUrl = "https://www.diving-fish.com/api/maimaidxprober/music_data";
 
-    private static _imageUrl = "https://www.diving-fish.com/covers/";
-
     public static fetch(): AxiosPromise<IMusicInfo[]> {
         return axios({
             url: this._dataUrl,
             method: "GET",
         });
-    }
-
-    public static fetchCover(musicId: number): string {
-        return this._imageUrl + musicId + ".jpg";
     }
 
 }
