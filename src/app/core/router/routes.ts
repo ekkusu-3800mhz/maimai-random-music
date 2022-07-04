3e9a4c2d24b08df21345f403552ff9a52f55345a
Shinra DX <shinra.dx@outlook.com>
2022-07-05 00:59:26 +0800

    完善系统路由

--- src/app/core/router/routes.ts
+++ src/app/core/router/routes.ts
@@ -1,13 +1,20 @@
 import { RouteRecordRaw } from "vue-router";
 
 import Intro from "@/app/view/Intro.vue";
+import BP from "@/app/view/BP.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
-        name: "Home",
+        name: "Intro",
         component: Intro,
     },
+    {
+        path: "/bp/:level",
+        name: "BP",
+        component: BP,
+        props: true,
+    }
 ];
 
 export default routes;
