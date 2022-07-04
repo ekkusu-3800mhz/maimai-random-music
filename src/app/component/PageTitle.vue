ea08363bca969d205aab95acd1f37aab615500ce
Shinra DX <shinra.dx@outlook.com>
2022-07-05 01:01:47 +0800

    定义BP环节页面标题组件

---
+++ src/app/component/PageTitle.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { injectionKey } from "@/app/core/store";
+import { ArrowLeft } from "@element-plus/icons-vue";
+
+const { push } = useRouter();
+const store = useStore(injectionKey);
+
+function back(): void {
+    store.commit("clear");
+    push({
+        name: "Intro",
+    });
+}
+</script>
+
+<template>
+    <el-row class="page-title" justify="center">
+        <el-col :xs="24" :sm="24">
+            <el-card>
+                <el-row>
+                    <el-col :xs="20" :sm="16">
+                        <el-page-header :icon="ArrowLeft" content="Ban & Pick环节" @back="back()" />
+                    </el-col>
+                </el-row>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<style lang="less" scoped>
+.page-title {
+    margin-top: -2rem;
+    padding-bottom: 1rem;
+}
+@media (min-width: 768px) {
+    .page-title {
+        margin-top: 0.5rem;
+    }
+}
+</style>
