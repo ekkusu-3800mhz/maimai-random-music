75a562e70a953092e5e15e66aa546be1d6643d99
Shinra DX <shinra.dx@outlook.com>
2022-07-05 01:03:14 +0800

    设置全局的CSS样式；设置Element Plus组件语言为中文

--- src/app/view/App.vue
+++ src/app/view/App.vue
@@ -1,3 +1,24 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { ElConfigProvider } from "element-plus";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+
+const locale = ref(zhCn);
+</script>
+
 <template>
-    <router-view />
+    <ElConfigProvider :locale="locale">
+        <router-view />
+    </ElConfigProvider>
 </template>
+
+<style lang="less">
+body {
+    background-color: #fcfcfc;
+    font-family: Roboto, Noto Sans SC, Noto Sans JP, Microsoft YaHei, '微软雅黑', Arial, sans-serif !important;
+    font-weight: 300;
+}
+img {
+    pointer-events: none;
+}
+</style>
