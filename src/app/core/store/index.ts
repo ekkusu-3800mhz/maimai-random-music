28e27723bdad2d7b6fb6e4df0a8b974d53fc4c23
Shinra DX <shinra.dx@outlook.com>
2022-07-12 11:32:56 +0800

    Set form values and mutation methods

--- src/app/core/store/index.ts
+++ src/app/core/store/index.ts
@@ -9,32 +9,54 @@ import { IMusicData, IMusicInfo } from "@/app/data/interface";
  *  @license MIT
  */
 
-export const injectionKey: InjectionKey<Store<IMusicData>> = Symbol();
+interface State extends IMusicData {
+    levelInput: string;
+    playerBan: number;
+    remainSong: number;
+    randomSong: number;
+}
 
-export default createStore<IMusicData>({
+export const injectionKey: InjectionKey<Store<State>> = Symbol();
+
+export default createStore<State>({
     state() {
         return {
             updateDate: "",
             version: "",
             musicList: [],
+            levelInput: "",
+            playerBan: 2,
+            remainSong: 3,
+            randomSong: 3,
         };
     },
     mutations: {
-        setUpdateDate(state: IMusicData, payload: string) {
+        setUpdateDate(state: State, payload: string) {
             state.updateDate = payload;
         },
-        setVersion(state: IMusicData, payload: string) {
+        setVersion(state: State, payload: string) {
             state.version = payload;
         },
-        setMusicList(state: IMusicData, payload: IMusicInfo[]) {
+        setMusicList(state: State, payload: IMusicInfo[]) {
             state.musicList = payload;
         },
-        clear(state: IMusicData) {
-            state = {
-                updateDate: "",
-                version: "",
-                musicList: [],
-            };
+        setLevelInput(state: State, payload: string) {
+            state.levelInput = payload;
+        },
+        setRandomSong(state: State, payload: number) {
+            state.randomSong = payload;
+        },
+        setPlayerBan(state: State, payload: number) {
+            state.playerBan = payload;
+        },
+        setRemainSong(state: State, payload: number) {
+            state.remainSong = payload;
+        },
+        clear(state: State) {
+            state.updateDate = "";
+            state.version = "";
+            state.musicList = [];
+            state.levelInput = "";
         },
     },
 });
