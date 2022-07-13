eec5ef22ae0eb08f0946e4a4511210905116b59b
Shinra DX <shinra.dx@outlook.com>
2022-07-13 10:18:25 +0800

    Import Element+ grid system

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "@/app/core/router";
 import store, { injectionKey } from "@/app/core/store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/display.css";
 
 /**
  *  清远舞萌比赛BP机制抽歌器 - SPA入口文件
